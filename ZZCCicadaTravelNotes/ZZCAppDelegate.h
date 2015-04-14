906bc6ed6ff940eff785519a2b34180e91954df3
zzc <zzc@zzctekimac.local>
2015-04-14 08:42:05 -0700

    Initial Commit

---
+++ ZZCCicadaTravelNotes/ZZCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZZCAppDelegate.h
+//  ZZCCicadaTravelNotes
+//
+//  Created by zzc on 15-4-14.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZZCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZZCCicadaTravelNotesTests/ZZCCicadaTravelNotesTests.m
@@ -0,0 +1,34 @@
+//
+//  ZZCCicadaTravelNotesTests.m
+//  ZZCCicadaTravelNotesTests
+//
+//  Created by zzc on 15-4-14.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ZZCCicadaTravelNotesTests : XCTestCase
+
+@end
+
+@implementation ZZCCicadaTravelNotesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
